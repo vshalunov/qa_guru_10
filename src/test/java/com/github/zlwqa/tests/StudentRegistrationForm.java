fd753f521f203551a10c579a062a8504e9caafe9
zlw-qa <asdori95@gmail.com>
2021-11-23 00:40:19 +0400

    fix gradle and add displayname

--- src/test/java/com/github/zlwqa/tests/StudentRegistrationForm.java
+++ src/test/java/com/github/zlwqa/tests/StudentRegistrationForm.java
@@ -1,11 +1,12 @@
 package com.github.zlwqa.tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.github.zlwqa.tests.TestData.studentNameResultField;
 import static com.github.zlwqa.tests.TestData.*;
 
-
+@DisplayName("Проверка формы Student Registration Form")
 public class StudentRegistrationForm extends TestBase {
 
 
